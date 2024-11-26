1d11bbba753dd0ee378aa37afe455599f521918d
Ryleeau <66577289+Ryleeau@users.noreply.github.com>
2024-11-26 11:42:36 -1000

    fixed lint errors

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,7 +1,7 @@
 const AddSpot = () => (
-    <div>
-      <h1>Favorite Spots Page</h1>
-    </div>
-  );
-  
-  export default AddSpot;
+  <div>
+    <h1>Favorite Spots Page</h1>
+  </div>
+);
+
+export default AddSpot;
